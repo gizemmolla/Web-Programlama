30e098102b4c429f3ce90f590010c8373047408b
gizemmolla <161225289+gizemmolla@users.noreply.github.com>
2025-12-15 23:07:57 +0300

    Update ApplicationDbContext.cs

--- webprogdeneme/Data/ApplicationDbContext.cs
+++ webprogdeneme/Data/ApplicationDbContext.cs
@@ -26,11 +26,13 @@ public static class DbSeeder
 
         var hizmet1 = new Hizmet { Ad = "Fitness", SureDakika = 60, Ucret = 250, SalonId = salon.Id };
         var hizmet2 = new Hizmet { Ad = "Yoga",    SureDakika = 45, Ucret = 200, SalonId = salon.Id };
-        db.Hizmetler.AddRange(hizmet1, hizmet2);
+        var hizmet3 = new Hizmet { Ad = "Pilates",    SureDakika = 60, Ucret = 300, SalonId = salon.Id };
+        db.Hizmetler.AddRange(hizmet1, hizmet2, hizmet3);
 
         var ant1 = new Antrenor { AdSoyad = "Ayşe Koç", Uzmanlik = "Kilo verme", MusaitlikSaatleri = new() { "2025-12-15 10:00", "2025-12-15 11:00" } };
         var ant2 = new Antrenor { AdSoyad = "Emre Demir", Uzmanlik = "Kas geliştirme", MusaitlikSaatleri = new() { "2025-12-15 14:00" } };
-        db.Antrenorler.AddRange(ant1, ant2);
+        var ant3 = new Antrenor { AdSoyad = "Salih Kaya", Uzmanlik = "Kas geliştirme", MusaitlikSaatleri = new() { "2025-12-15 17:00" } };
+        db.Antrenorler.AddRange(ant1, ant2, ant3);
 
         var uye = new Uye { AdSoyad = "Test Uye", Email = "test@example.com" };
         db.Uyeler.Add(uye);
