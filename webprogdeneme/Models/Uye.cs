c7f46b0e73899bec78308b66d22edbc0077830f9
gizemmolla <161225289+gizemmolla@users.noreply.github.com>
2025-12-16 23:00:53 +0300

    Update Uye.cs

--- webprogdeneme/Models/Uye.cs
+++ webprogdeneme/Models/Uye.cs
@@ -2,6 +2,7 @@ namespace FitnessCenter.Models;
 public class Uye
 {
     public int Id { get; set; }
-    public string AdSoyad { get; set; } = string.Empty;
+    public string Ad { get; set; } = string.Empty;
+    public string Soyad { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
 }
